2f25d0bffce1c7d86a8dd0e1fc41b1859f4220bd
Aayush1125 <137314280+Aayush1125@users.noreply.github.com>
2025-07-01 19:11:17 +0530

    Update INDIA.py

--- INDIA.py
+++ INDIA.py
@@ -613,10 +613,10 @@ def show_india_timelapse_map(df, geojson_path, metric_title="Production", defaul
 
 with open("states_india.geojson", "r", encoding="utf-8") as f:
     india_geojson = json.load(f)
-
+'''
 # Print the first feature
 first_feature = india_geojson["features"][0]
 st.write(first_feature)
-
+'''
 
 
