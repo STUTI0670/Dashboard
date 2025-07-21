8c181adc0fb3e79f6fe8be6769846c74ebe653c9
Dhruva Sharma <91600974+DhruvaSharma2003@users.noreply.github.com>
2025-07-21 19:06:57 +0530

    Update INDIA.py

--- INDIA.py
+++ INDIA.py
@@ -312,7 +312,7 @@ try:
         edgecolor='black',
         missing_kwds={"color": "white", "edgecolor": "black"}
     )
-    plt.title(f"{pulse_type} - {season} - {metric} in {selected_year}", fontsize=12)
+    ax.set_title(f"{pulse_type} - {season} - {metric} in {selected_year}", fontsize=12)
     st.pyplot(fig)
 
 
