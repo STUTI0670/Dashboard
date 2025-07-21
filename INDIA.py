4f190b8bace07854653278227c1486fe4ed27ea7
Dhruva Sharma <91600974+DhruvaSharma2003@users.noreply.github.com>
2025-07-21 19:11:29 +0530

    Update INDIA.py

--- INDIA.py
+++ INDIA.py
@@ -310,7 +310,7 @@ try:
         legend=True,
         cmap='YlOrRd',
         
-        missing_kwds={"color": "white", "edgecolor": "black"}
+        missing_kwds={"color": "black", "edgecolor": "white"}
     )
     ax.set_title(f"{pulse_type} - {season} - {metric} in {selected_year}", fontsize=12)
     st.pyplot(fig)
